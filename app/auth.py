c8da9c28a0423dd6ec621b2e05d893adf94f605c
Tim Harvey <timharveydev@protonmail.com>
2021-11-17 21:39:07 +0000

    Added GET and POST method permissions to relevant routes.

--- app/auth.py
+++ app/auth.py
@@ -6,3 +6,11 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login-register')
 def loginRegister():
   return render_template('login_register.html')
+
+@auth.route('/login-request', methods=['GET', 'POST'])
+def loginRequest():
+  return render_template('login_register.html')
+
+@auth.route('/register-request', methods=['GET', 'POST'])
+def registerRequest():
+  return render_template('login_register.html')
--- app/views.py
+++ app/views.py
@@ -7,11 +7,11 @@ views = Blueprint('views', __name__)
 def home():
   return render_template('home.html')
 
-@views.route('/collection')
+@views.route('/collection', methods=['GET', 'POST'])
 def myCollection():
   return render_template('collection.html')
 
-@views.route('/comic-details')
+@views.route('/comic-details', methods=['GET', 'POST'])
 def comicDetails():
   return render_template('comic_details.html')
 
@@ -19,6 +19,6 @@ def comicDetails():
 def comics():
   return render_template('comics.html')
 
-@views.route('/contact')
+@views.route('/contact', methods=['GET', 'POST'])
 def contact():
   return render_template('contact.html')
